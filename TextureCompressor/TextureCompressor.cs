e572cceb33ced15bb56a4914107f526a2a2c1517
waka324 <rbray89@gmail.com>
2013-11-29 22:30:16 -0700

    Move to compress during load.
    Changes derived from user sarbian on commit
    f61eb139a4ecd55df2998b8a4ec9205bda66de8d

--- TextureCompressor/TextureCompressor.cs
+++ TextureCompressor/TextureCompressor.cs
@@ -10,43 +10,93 @@ namespace TextureCompressor
     public class TextureCompressor : MonoBehaviour
     {
         static bool Compressed = false;
+        static int LastTextureIndex = -1;
+        static int memorySaved = 0;
 
         protected void Awake()
         {
-
-            if (HighLogic.LoadedScene == GameScenes.MAINMENU)
+            if (HighLogic.LoadedScene == GameScenes.MAINMENU && !Compressed)
             {
-                CompressAllTextures();
+                Update();
+                Compressed = true;
+                int kbSaved = (int) (memorySaved / 1024f);
+                int mbSaved = (int)((memorySaved / 1024f) / 1024f);
+                Log("Memory Saved : "+ memorySaved.ToString() + "B");
+                Log("Memory Saved : " + kbSaved.ToString() + "kB");
+                Log("Memory Saved : " + mbSaved.ToString() + "MB");
             }
         }
 
-        private void CompressAllTextures()
+        protected void Update()
         {
-            if(!Compressed)
+            if (!Compressed && GameDatabase.Instance.databaseTexture.Count > 0)
             {
-                foreach (GameDatabase.TextureInfo t in GameDatabase.Instance.databaseTexture)
+                int LocalLastTextureIndex = GameDatabase.Instance.databaseTexture.Count-1;
+                if (LastTextureIndex != LocalLastTextureIndex)
                 {
-                    //Log("name: " + t.name);
-                    //Log("format: " + t.texture.format.ToString());
-                    try
+                    for (int i = LastTextureIndex + 1; i < GameDatabase.Instance.databaseTexture.Count; i++)
                     {
-                        t.texture.GetPixel(0,0);
-                        if (t.isReadable && t.texture.format.ToString() != TextureFormat.DXT1.ToString() && t.texture.format.ToString() != TextureFormat.DXT5.ToString())
-                        {
-                            Log("Compressing... " + t.name);
-                            t.texture.Compress(true);
-                        }
+                        GameDatabase.TextureInfo Texture = GameDatabase.Instance.databaseTexture[i];
+                        LastTextureIndex = i;
+                        CompressTexture(Texture.texture);
                     }
-                    catch(UnityException e)
-                    {
-
-                    }
-                    
                 }
-
-
-                Compressed = true;
             }
+        }
+
+        private void CompressTexture(Texture2D texture)
+        {
+            try
+            {
+                TextureFormat format = texture.format;
+
+                if (format != TextureFormat.DXT1 && format != TextureFormat.DXT5)
+                {
+                    texture.GetPixel(0, 0);
+                    Log("--------------------------------------------------------");
+                    Log("Name: " + texture.name);
+                    Log("Format: " + texture.format.ToString());
+                    Log("Size: " + texture.width.ToString() + "x" + texture.height);
+                    texture.Compress(true);
+                    TextureFormat newFormat = texture.format;
+                    int oldSize = 0;
+                    int newSize = 0;
+                    switch (format)
+                    {
+                        case TextureFormat.ARGB32:
+                        case TextureFormat.RGBA32:
+                        case TextureFormat.BGRA32:
+                            oldSize = 4 * (texture.width * texture.height);
+                            break;
+                        case TextureFormat.RGB24:
+                            oldSize = 3 * (texture.width * texture.height);
+                            break;
+                        case TextureFormat.Alpha8:
+                            oldSize = texture.width * texture.height;
+                            break;
+                    }
+                    if (newFormat == TextureFormat.DXT1)
+                    {
+                        newSize = (texture.width * texture.height) / 2;
+                    }
+                    else
+                    {
+                        newSize = (texture.width * texture.height);
+                    }
+                    int saved = (oldSize - newSize);
+                    Log("Saved: " + saved.ToString() + "B");
+                    memorySaved += saved;
+                }
+                else
+                {
+                    //Log("Already Compressed.");
+                }
+            }
+            catch (UnityException e)
+            {
+
+            }
+
         }
 
         public static void Log(String message)
