4eb66e906d293c0a4db3454d2b4c235c55ed2047
waka324 <rbray89@gmail.com>
2013-12-28 16:52:15 -0700

    Added config, Texture Resizing, MBM replacement.

--- TextureCompressor/TextureCompressor.cs
+++ TextureCompressor/TextureCompressor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using UnityEngine;
@@ -10,8 +11,26 @@ namespace TextureCompressor
     public class TextureCompressor : MonoBehaviour
     {
         static bool Compressed = false;
+        static bool Converted = false;
         static int LastTextureIndex = -1;
         static int memorySaved = 0;
+        const int MAX_IMAGE_SIZE = 4048*4048*4;
+        static byte[] imageBuffer = new byte[MAX_IMAGE_SIZE];
+        static ConfigNode config;
+        static ConfigNode overrides;
+        static ConfigNode overridesFolders;
+        static List<String> overridesFolderList = new List<string>();
+
+        static bool mipmaps = false;
+        static bool compress = true;
+        static bool discard_alpha = false;
+        static int scale = 1;
+        static int max_size = 1;
+        static bool mipmaps_normals = false;
+        static bool compress_normals = true;
+        static bool discard_alpha_normals = false;
+        static int scale_normals = 1;
+        static int max_size_normals = 1;
 
         protected void Awake()
         {
@@ -24,11 +43,214 @@ namespace TextureCompressor
                 Log("Memory Saved : "+ memorySaved.ToString() + "B");
                 Log("Memory Saved : " + kbSaved.ToString() + "kB");
                 Log("Memory Saved : " + mbSaved.ToString() + "MB");
+                foreach(GameDatabase.TextureInfo Texture in GameDatabase.Instance.databaseTexture)
+                {
+                    Texture2D texture = Texture.texture;
+                    Log("--------------------------------------------------------");
+                    Log("Name: " + texture.name);
+                    Log("Format: " + texture.format.ToString());
+                    Log("MipMaps: " + texture.mipmapCount.ToString());
+                    Log("Size: " + texture.width.ToString() + "x" + texture.height);
+                }
             }
+            if(!Converted)
+            {
+                String[] allfiles = System.IO.Directory.GetFiles(KSPUtil.ApplicationRootPath + "GameData/", "*.mbm", System.IO.SearchOption.AllDirectories);
+                
+                foreach (String file in allfiles)
+                {
+                    FileStream stream = new FileStream(file, FileMode.Open, FileAccess.ReadWrite);
+                    stream.Position = 12;
+                    if(stream.ReadByte() == 1)
+                    {
+                        stream.Position = 12;
+                        //stream.WriteByte(0x00);
+                        String unixPath = file.Replace('\\', '/');
+                        stream.Close();
+                        //MBMToPNG(file);
+                        MBMToTGA(file);
+                        File.Move(file, file+".origN");
+                    }
+                    else
+                    {
+                        stream.Close();
+                        MBMToPNG(file);
+                        //MBMToTGA(file);
+                        File.Move(file, file + ".orig");
+                    }
+                }
+                imageBuffer = null;
+                Converted = true;
+            }
+        }
+
+        private void MBMToPNG(string file)
+        {
+            FileStream mbmStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+            mbmStream.Position = 4;
+
+            uint width = 0, height = 0;
+            for (int b = 0; b < 4; b++)
+            {
+                width >>= 8;
+                width |= (uint)(mbmStream.ReadByte() << 24);
+            }
+            for (int b = 0; b < 4; b++)
+            {
+                height >>= 8;
+                height |= (uint)(mbmStream.ReadByte() << 24);
+            }
+            mbmStream.Position = 16;
+            int format = mbmStream.ReadByte();
+            mbmStream.Position += 3;
+            //Log(file+" width: " + width.ToString() + " height: " + height.ToString() + " format: " + format.ToString());
+
+            int imageSize = (int)(width * height * 3);
+            TextureFormat texformat = TextureFormat.RGB24;
+            bool alpha = false;
+            if (format == 32)
+            {
+                imageSize += (int)(width * height);
+                texformat = TextureFormat.RGBA32;
+                alpha = true;
+            }
+
+            String pngOut = file.Substring(0, file.Length - 3) + "png";
+            mbmStream.Read(imageBuffer, 0, MAX_IMAGE_SIZE);
+            mbmStream.Close();
+
+            Texture2D texture = new Texture2D((int)width, (int)height, texformat, false);
+            Color32[] colors = new Color32[width * height];
+            int n = 0;
+            for (int i = 0; i < width * height; i++ )
+            {
+                colors[i].r = imageBuffer[n++];
+                colors[i].g = imageBuffer[n++];
+                colors[i].b = imageBuffer[n++];
+                if(alpha)
+                {
+                    colors[i].a = imageBuffer[n++];
+                }
+            }
+            texture.SetPixels32(colors);
+            texture.Apply(false);
+            byte[] data = texture.EncodeToPNG();
+            File.WriteAllBytes(pngOut, data);
+        }
+
+        private void MBMToTGA(string file)
+        {
+            FileStream mbmStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+            mbmStream.Position = 4;
+            
+            uint width  = 0, height = 0;
+            for(int b=0; b < 4; b++)
+            {
+                width >>= 8;
+                width |= (uint)(mbmStream.ReadByte() << 24);
+            }
+            for(int b=0; b < 4; b++)
+            {
+                height >>= 8;
+                height |= (uint)(mbmStream.ReadByte() << 24);
+            }
+            mbmStream.Position = 16;
+            int format = mbmStream.ReadByte();
+            mbmStream.Position += 3;
+            //Log(file+" width: " + width.ToString() + " height: " + height.ToString() + " format: " + format.ToString());
+            bool alpha = false;
+            int imageSize = (int)(width * height * 3);
+            if(format == 32)
+            {
+                alpha = true;
+                imageSize += (int)(width * height);
+            }
+            
+            String tgaOut = file.Substring(0, file.Length - 3) + "tga";
+            FileStream tgaStream = new FileStream(tgaOut, FileMode.OpenOrCreate, FileAccess.Write);
+            tgaStream.SetLength(0);
+            tgaStream.Write(new byte[] {
+                0x00,
+                0x00,
+                0x02,
+                0x00,0x00,0x00,0x00,0x00,
+                0x00,0x00,
+                0x00,0x00
+                }, 0, 12);
+            tgaStream.WriteByte((byte)(width & 0xFF));
+            tgaStream.WriteByte((byte)(width >> 8));
+            tgaStream.WriteByte((byte)(height & 0xFF));
+            tgaStream.WriteByte((byte)(height >> 8));
+            tgaStream.WriteByte((byte)format);
+            if (alpha)
+            {
+                tgaStream.WriteByte(0x08);
+            }
+            else
+            {
+                tgaStream.WriteByte(0x00);
+            }
+            
+            mbmStream.Read(imageBuffer,0,MAX_IMAGE_SIZE);
+            mbmStream.Close();
+            int inc = alpha ? 4 : 3;
+            for (int i = 0; i < imageSize; i += inc)
+            {
+                byte r = imageBuffer[i];
+                imageBuffer[i] = imageBuffer[i+2];
+                imageBuffer[i + 2] = r;
+            }
+            tgaStream.Write(imageBuffer, 0, imageSize);
+            /*
+            String headerTxt = "TRUEVISION-XFILE.";
+            byte[] footer = new byte[9+headerTxt.Length];
+            Encoding.ASCII.GetBytes("TRUEVISION-XFILE.").CopyTo(footer,8);
+            tgaStream.Write(footer, 0, 8);
+            */
+            tgaStream.Flush();
+            tgaStream.Close();
         }
 
         protected void Update()
         {
+            if (config == null)
+            {
+                String configString = KSPUtil.ApplicationRootPath + "GameData/BoulderCo/textureCompressor.cfg";
+                ConfigNode settings = ConfigNode.Load(configString);
+                config = settings.GetNode("COMPRESSOR");
+                overrides = settings.GetNode("OVERRIDES");
+                overridesFolders = settings.GetNode("OVERRIDES_FOLDERS");
+                foreach (ConfigNode node in overridesFolders.nodes)
+                {
+                    overridesFolderList.Add(node.name);
+                }
+
+
+                String mipmapsString = config.GetValue("mipmaps");
+                String compressString = config.GetValue("compress");
+                String discard_alphaString = config.GetValue("discard_alpha");
+                String scaleString = config.GetValue("scale");
+                String max_sizeString = config.GetValue("max_size");
+                
+                bool.TryParse(mipmapsString, out mipmaps);
+                bool.TryParse(compressString, out compress);
+                bool.TryParse(discard_alphaString, out discard_alpha);
+                int.TryParse(scaleString, out scale);
+                int.TryParse(max_sizeString, out max_size);
+
+                String mipmapsString_normals = config.GetValue("mipmaps_normals");
+                String compressString_normals = config.GetValue("compress_normals");
+                String discard_alphaString_normals = config.GetValue("discard_alpha_normals");
+                String scaleString_normals = config.GetValue("scale_normals");
+                String max_sizeString_normals = config.GetValue("max_size_normals");
+                
+                bool.TryParse(mipmapsString_normals, out mipmaps_normals);
+                bool.TryParse(compressString_normals, out compress_normals);
+                bool.TryParse(discard_alphaString_normals, out discard_alpha_normals);
+                int.TryParse(scaleString_normals, out scale_normals);
+                int.TryParse(max_sizeString_normals, out max_size_normals);
+            }
+
             if (!Compressed && GameDatabase.Instance.databaseTexture.Count > 0)
             {
                 int LocalLastTextureIndex = GameDatabase.Instance.databaseTexture.Count-1;
@@ -38,63 +260,195 @@ namespace TextureCompressor
                     {
                         GameDatabase.TextureInfo Texture = GameDatabase.Instance.databaseTexture[i];
                         LastTextureIndex = i;
-                        CompressTexture(Texture.texture);
+                        String path = KSPUtil.ApplicationRootPath + "GameData/" + Texture.name + ".mbm";
+                        if (File.Exists(path+".origN"))
+                        {
+                            Texture.isNormalMap = true;
+                        }
+                        else
+                        {
+                            Texture.isNormalMap = false;
+                        }
+                        ConfigNode overrideNode = overrides.GetNode(Texture.name);
+                        string folder = overridesFolderList.Find(n => Texture.name.StartsWith(n));
+                        if(overrideNode != null)
+                        {
+                            ApplyNodeSettings(Texture, overrideNode);
+                        }
+                        else if(folder != null)
+                        {
+                            ConfigNode overrideFolder = overridesFolders.GetNode(folder);
+                        }
+                        else 
+                        {
+                            ApplySettings(Texture);
+                        }
                     }
                 }
             }
         }
 
-        private void CompressTexture(Texture2D texture)
+        private void ApplyNodeSettings(GameDatabase.TextureInfo Texture, ConfigNode overrideNode)
         {
-            try
-            {
-                TextureFormat format = texture.format;
+            String mipmapsString = overrideNode.GetValue("mipmaps");
+            String compressString = overrideNode.GetValue("compress");
+            String discard_alphaString = overrideNode.GetValue("discard_alpha");
+            String scaleString = overrideNode.GetValue("scale");
+            bool mipmaps = false;
+            bool compress = true;
+		    bool discard_alpha = false;
+            int scale = 1;
+            bool.TryParse(mipmapsString, out mipmaps);
+            bool.TryParse(compressString, out compress);
+            bool.TryParse(discard_alphaString, out discard_alpha);
+            int.TryParse(scaleString, out scale);
 
-                if (format != TextureFormat.DXT1 && format != TextureFormat.DXT5)
+            Texture2D tex = Texture.texture;
+            TextureFormat format = tex.format;
+            if (discard_alpha || format == TextureFormat.DXT1 || format == TextureFormat.RGB24)
+            {
+                format = TextureFormat.RGB24;
+            }
+            else
+            {
+                format = TextureFormat.RGBA32;
+            }
+
+            UpdateTex(Texture, format, compress, mipmaps, scale);
+
+        }
+
+        private void ApplySettings(GameDatabase.TextureInfo Texture)
+        {
+            bool mipmaps = TextureCompressor.mipmaps;
+            bool compress = TextureCompressor.compress;
+            bool discard_alpha = TextureCompressor.discard_alpha;
+            int scale = TextureCompressor.scale;
+            int max_size = TextureCompressor.max_size;
+            if(Texture.isNormalMap)
+            {
+                mipmaps = TextureCompressor.mipmaps_normals;
+                compress = TextureCompressor.compress_normals;
+                discard_alpha = TextureCompressor.discard_alpha_normals;
+                scale = TextureCompressor.scale_normals;
+                max_size = TextureCompressor.max_size_normals;
+            }
+            Texture2D tex = Texture.texture;
+            TextureFormat format = tex.format;
+            if (discard_alpha || format == TextureFormat.DXT1 || format == TextureFormat.RGB24)
+            {
+                format = TextureFormat.RGB24;
+            }
+            else
+            {
+                format = TextureFormat.RGBA32;
+            }
+
+            UpdateTex(Texture, format, compress, mipmaps, scale, max_size);
+
+        }
+
+        private void UpdateTex(GameDatabase.TextureInfo Texture, TextureFormat format, bool compress, bool mipmaps, int scale, int max_size = 0)
+        {
+            Texture2D tex = Texture.texture;
+            TextureFormat originalFormat = tex.format;
+            int originalWidth = tex.width;
+            int originalHeight = tex.height;
+            int width = tex.width;
+            int height = tex.height;
+            bool hasMipmaps = tex.mipmapCount == 1 ? false : true;
+            if ((mipmaps || format != originalFormat) && scale == 1 && (max_size == 0 || (width <= max_size && height <= max_size)))
+            {
+                Color32[] pixels = tex.GetPixels32();
+                tex.Resize(width, height, format, mipmaps);
+                tex.SetPixels32(pixels);
+                tex.Apply(mipmaps);
+            }
+            else
+            {
+                width = tex.width / scale;
+                height = tex.height / scale;
+                if (max_size != 0)
                 {
-                    texture.GetPixel(0, 0);
-                    Log("--------------------------------------------------------");
-                    Log("Name: " + texture.name);
-                    Log("Format: " + texture.format.ToString());
-                    Log("Size: " + texture.width.ToString() + "x" + texture.height);
-                    texture.Compress(true);
-                    TextureFormat newFormat = texture.format;
-                    int oldSize = 0;
-                    int newSize = 0;
-                    switch (format)
+                    if(width > max_size)
                     {
-                        case TextureFormat.ARGB32:
-                        case TextureFormat.RGBA32:
-                        case TextureFormat.BGRA32:
-                            oldSize = 4 * (texture.width * texture.height);
-                            break;
-                        case TextureFormat.RGB24:
-                            oldSize = 3 * (texture.width * texture.height);
-                            break;
-                        case TextureFormat.Alpha8:
-                            oldSize = texture.width * texture.height;
-                            break;
+                        width = max_size;
                     }
-                    if (newFormat == TextureFormat.DXT1)
+                    if(height > max_size)
                     {
-                        newSize = (texture.width * texture.height) / 2;
+                        height = max_size;
                     }
-                    else
-                    {
-                        newSize = (texture.width * texture.height);
-                    }
-                    int saved = (oldSize - newSize);
-                    Log("Saved: " + saved.ToString() + "B");
-                    memorySaved += saved;
+                }
+                TextureResizer.Resize(tex, width, height, format, mipmaps);
+            }
+            
+            if (compress)
+            {
+                tex.Compress(true);
+            }
+
+            String path = KSPUtil.ApplicationRootPath + "GameData/" + Texture.name + ".mbm";
+            if (!File.Exists(path + ".orig"))
+            {
+                if (originalFormat == TextureFormat.RGB24)
+                {
+                    originalFormat = TextureFormat.DXT1;
                 }
                 else
                 {
-                    //Log("Already Compressed.");
+                    originalFormat = TextureFormat.RGBA32;
                 }
             }
-            catch (UnityException e)
-            {
 
+            int oldSize = 0;
+            int newSize = 0;
+            switch (originalFormat)
+            {
+                case TextureFormat.ARGB32:
+                case TextureFormat.RGBA32:
+                case TextureFormat.BGRA32:
+                    oldSize = 4 * (originalWidth * originalHeight);
+                    break;
+                case TextureFormat.RGB24:
+                    oldSize = 3 * (originalWidth * originalHeight);
+                    break;
+                case TextureFormat.Alpha8:
+                    oldSize = originalWidth * originalHeight;
+                    break;
+            }
+            switch (tex.format)
+            {
+                case TextureFormat.ARGB32:
+                case TextureFormat.RGBA32:
+                case TextureFormat.BGRA32:
+                    newSize = 4 * (width * height);
+                    break;
+                case TextureFormat.RGB24:
+                    newSize = 3 * (width * height);
+                    break;
+                case TextureFormat.Alpha8:
+                    newSize = width * height;
+                    break;
+                case TextureFormat.DXT1:
+                    newSize = (width * height) / 2;
+                    break;
+                case TextureFormat.DXT5:
+                    newSize = width * height;
+                    break;
+            }
+            if(hasMipmaps)
+            {
+                oldSize += (int)(oldSize * .33f);
+            }
+            if(mipmaps)
+            {
+                newSize += (int)(newSize * .33f);
+            }
+            int saved = (oldSize - newSize);
+            if (saved > 0)
+            {
+                Log("Saved: " + saved.ToString() + "B");
+                memorySaved += saved;
             }
 
         }
---
+++ TextureCompressor/TextureResizer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace TextureCompressor
+{
+    class TextureResizer
+    {
+        public static void Resize(Texture2D texture, int width, int height, TextureFormat format, bool mipmaps)
+        {
+            Color32[] pixels = texture.GetPixels32();
+            int origWidth = texture.width;
+            int origHeight = texture.height;
+            Color32[] newPixels = new Color32[width * height];
+            int index = 0;
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    newPixels[index] = GetPixel(pixels, texture, ((float)w) / width, ((float)h) / height, width, height);
+                    index++;
+                }
+            }
+            texture.Resize(width, height, format, mipmaps);
+            texture.SetPixels32(pixels);
+            texture.Apply(mipmaps);
+        }
+
+        private static Color32 GetPixel(Color32[] pixels, Texture2D tex, float w, float h, float width, float height)
+        {
+            float widthDist = 4.0f - ((4.0f * (float)width) / tex.width);
+            float heightDist = 4.0f - ((4.0f * (float)height) / tex.height);
+            int[,] posArray = new int[2, 4];
+            posArray[0, 0] = (int)Math.Floor((w * tex.width) - widthDist);
+            posArray[0, 1] = (int)Math.Floor(w * tex.width);
+            posArray[0, 2] = (int)Math.Ceiling((w * tex.width) + widthDist);
+            posArray[0, 3] = (int)Math.Ceiling((w * tex.width) + (2.0 * widthDist));
+            posArray[1, 0] = (int)Math.Floor((h * tex.height) - heightDist);
+            posArray[1, 1] = (int)Math.Floor(h * tex.height);
+            posArray[1, 2] = (int)Math.Ceiling((h * tex.height) + heightDist);
+            posArray[1, 3] = (int)Math.Ceiling((h * tex.height) + (2.0 * heightDist));
+
+            Color32 cw1 = new Color32(), cw2 = new Color32(), cw3 = new Color32(), cw4 = new Color32(), ch1 = new Color32(), ch2 = new Color32(), ch3 = new Color32(), ch4 = new Color32();
+            int w1 = posArray[0, 0];
+            int w2 = posArray[0, 1];
+            int w3 = posArray[0, 2];
+            int w4 = posArray[0, 3];
+            int h1 = posArray[1, 0];
+            int h2 = posArray[1, 1];
+            int h3 = posArray[1, 2];
+            int h4 = posArray[1, 3];
+
+            if (h2 >= 0 && h2 < tex.height)
+            {
+                if (w2 >= 0 && w2 < tex.width)
+                {
+                    cw2 = pixels[w2+ (h2*tex.width)];
+                }
+                if (w1 >= 0 && w1 < tex.width)
+                {
+                    cw1 = pixels[w1 + (h2 * tex.width)];
+                }
+                else
+                {
+                    cw1 = cw2;
+                }
+                if (w3 >= 0 && w3 < tex.width)
+                {
+                    cw3 = pixels[w3 + (h2 * tex.width)];
+                }
+                else
+                {
+                    cw3 = cw2;
+                }
+                if (w4 >= 0 && w4 < tex.width)
+                {
+                    cw4 = pixels[w4 + (h2 * tex.width)];
+                }
+                else
+                {
+                    cw4 = cw3;
+                }
+
+            }
+            if (w2 >= 0 && w2 < tex.width)
+            {
+                if (h2 >= 0 && h2 < tex.height)
+                {
+                    ch2 = pixels[w2 + (h2 * tex.width)];
+                }
+                if (h1 >= 0 && h1 < tex.height)
+                {
+                    ch1 = pixels[w2 + (h1 * tex.width)];
+                }
+                else
+                {
+                    ch1 = ch2;
+                }
+                if (h3 >= 0 && h3 < tex.height)
+                {
+                    ch3 = pixels[w2 + (h3 * tex.width)];
+                }
+                else
+                {
+                    ch3 = ch2;
+                }
+                if (h4 >= 0 && h4 < tex.height)
+                {
+                    ch4 = pixels[w2 + (h4 * tex.width)];
+                }
+                else
+                {
+                    ch4 = ch3;
+                }
+            }
+            byte cwr = (byte)(((.25f * cw1.r) + (.75f * cw2.r) + (.75f * cw3.r) + (.25f * cw4.r)) / 2.0f);
+            byte cwg = (byte)(((.25f * cw1.g) + (.75f * cw2.g) + (.75f * cw3.g) + (.25f * cw4.g)) / 2.0f);
+            byte cwb = (byte)(((.25f * cw1.b) + (.75f * cw2.b) + (.75f * cw3.b) + (.25f * cw4.b)) / 2.0f);
+            byte cwa = (byte)(((.25f * cw1.a) + (.75f * cw2.a) + (.75f * cw3.a) + (.25f * cw4.a)) / 2.0f);
+            byte chr = (byte)(((.25f * ch1.r) + (.75f * ch2.r) + (.75f * ch3.r) + (.25f * ch4.r)) / 2.0f);
+            byte chg = (byte)(((.25f * ch1.g) + (.75f * ch2.g) + (.75f * ch3.g) + (.25f * ch4.g)) / 2.0f);
+            byte chb = (byte)(((.25f * ch1.b) + (.75f * ch2.b) + (.75f * ch3.b) + (.25f * ch4.b)) / 2.0f);
+            byte cha = (byte)(((.25f * ch1.a) + (.75f * ch2.a) + (.75f * ch3.a) + (.25f * ch4.a)) / 2.0f);
+            byte R = (byte)((cwr + chr) / 2.0f);
+            byte G = (byte)((cwg + chg) / 2.0f);
+            byte B = (byte)((cwb + chb) / 2.0f);
+            byte A = (byte)((cwa + cha) / 2.0f);
+
+            Color32 color = new Color32(R, G, B, A);
+            return color;
+        }
+    }
+}
